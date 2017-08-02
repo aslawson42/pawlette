267ae7444c1d51281b5b192200634cb2dee5acd0
Drew <andrewslawson42@gmail.com>
2017-08-02 11:17:49 -0400

    JS and Toggle Buttons

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("menuSideNav").style.width = "240px";
+}
+function closeNav() {
+  document.getElementById("menuSideNav").style.width = "0";
+}
+function toggleButton(x) {
+  x.classList.toggle("btn-active");
+}
